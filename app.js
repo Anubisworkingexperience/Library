f2c8eb76fdaeb0052fa9778569256f63b3b22307
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-03-02 00:41:06 +0300

    Add modal box book window

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const addButton = document.querySelector('.add');
+const addContainer = document.querySelector('.add-container');
+const dialog = document.querySelector('dialog');
+
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
 const myLibrary = [];
 
-function Book() {
-
+function Book(author, title, numberOfPages) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
 }
 
 function addBookToLibrary() {
