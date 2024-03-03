061ffe5670b3b0c82922a4c4b7a7bdf7b0d0ddf4
Anubisworkingexperience <79977978+Anubisworkingexperience@users.noreply.github.com>
2024-03-03 17:42:51 +0300

    Open/close modal book box

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const addButton = document.querySelector('.add');
 const addContainer = document.querySelector('.add-container');
 const dialog = document.querySelector('dialog');
+const body = document.querySelector('body');
+const content = document.querySelector('.content');
 
-addButton.addEventListener('click', () => {
-    dialog.showModal();
-});
+
+function modalBox() {
+    let modalOpened;
+    const modalElements = document.querySelectorAll('.modal');
+    console.log(modalElements);
+
+    addButton.addEventListener('click', () => {
+        dialog.showModal();
+        modalOpened = true;
+    });
+    dialog.addEventListener('click', (event) => {
+        if (modalOpened && !content.contains(event.target)) {
+            dialog.close();
+            modalOpened = false;
+        }
+    });
+}
+
+modalBox();
 
 const myLibrary = [];
 
